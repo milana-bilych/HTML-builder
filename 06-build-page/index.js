154003c1c5e5d3405f558e670e2fb86cb18e3bfe
milana-bilych <akimmilana@gmail.com>
2022-11-07 19:40:39 +0200

    fix: 6th task fixing

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,6 +6,28 @@ let second = path.resolve(__dirname, 'components');
 let total = path.resolve(__dirname, 'project-dist');
 let template = fs.createReadStream(first, 'utf8');
 
+fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, err => {
+    if(err) throw err;
+  });
+  
+  let styles = path.resolve(__dirname, 'styles');
+  let bundles = path.resolve(__dirname, 'project-dist', 'style.css');
+  
+  fs.readdir(styles, { withFileTypes: true }, function(err, items) {
+    if (err) {
+      throw err;
+    }
+    const out = fs.createWriteStream(bundles);
+    items.forEach(function(item) {
+      let parse = path.parse(item.name).ext;
+      if(item.isFile() === true && parse == '.css')  {
+        const input = fs.createReadStream(path.join(styles, item.name), 'utf-8');
+        input.on('data', n => out.write(n));
+        input.on('error', err  => console.log('Error', err.message));
+      }
+    });
+  });
+
 fs.readFile(first, function (err) {
   fs.mkdir(total, { recursive: true }, err => {
     if(err) throw err;
@@ -39,58 +61,36 @@ fs.readFile(first, function (err) {
   });
 });
 
-fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, err => {
-  if(err) throw err;
-});
-
-let styles = path.resolve(__dirname, 'styles');
-let bundles = path.resolve(__dirname, 'project-dist', 'style.css');
-
-fs.readdir(styles, { withFileTypes: true }, function(err, items) {
-  if (err) {
-    throw err;
-  }
-  const out = fs.createWriteStream(bundles);
-  items.forEach(function(item) {
-    let parse = path.parse(item.name).ext;
-    if(item.isFile() === true && parse == '.css')  {
-      const input = fs.createReadStream(path.join(styles, item.name), 'utf-8');
-      input.on('data', n => out.write(n));
-      input.on('error', err  => console.log('Error', err.message));
-    }
-  });
-});
-
-function copy(src, dist) {
-  fs.readdir(src, {withFileTypes: true }, (err, items) => {
-    if (err) {
-      throw err;
-    }
-    for (let item of items) {
-      if (item.isDirectory()) {
-        let copySrc = path.resolve(src, item.name);
-        let copyDist = path.resolve(dist, item.name);
-        copy(copySrc, copyDist);
-      } else {
-        fs.mkdir(dist, { recursive: true }, function(err) {
-          if (err) {
-            throw err;
-          }
-        });
-        fs.copyFile(
-          path.resolve(src, item.name),
-          path.resolve(dist, item.name), 
-          function (err) {
-            if (err)
-              throw err;
-          }
-        );
+function copyFiles(src, dist) {
+    fs.readdir(src, {withFileTypes: true }, (err, items) => {
+      if (err) {
+        throw err;
       }
-    }
-}); 
-}
+      for (let item of items) {
+        if (item.isDirectory()) {
+          let copySrc = path.resolve(src, item.name);
+          let copyDist = path.resolve(dist, item.name);
+          copyFiles(copySrc, copyDist);
+        } else {
+          fs.mkdir(dist, { recursive: true }, function(err) {
+            if (err) {
+              throw err;
+            }
+          });
+          fs.copyFile(
+            path.resolve(src, item.name),
+            path.resolve(dist, item.name), 
+            function (err) {
+              if (err)
+                throw err;
+            }
+          );
+        }
+      }
+  }); 
+  }
 
-copy(path.resolve(__dirname, 'assets'), path.resolve(total, 'assets'), function(err) {
+copyFiles(path.resolve(__dirname, 'assets'), path.resolve(total, 'assets'), function(err) {
   if (err) {
     throw err;
   }
